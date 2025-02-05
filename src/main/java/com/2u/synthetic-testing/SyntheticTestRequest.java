dabeed02b42951abe6ec5d925a7c9ff2cb5b455f
bszabo <bszabo@rcn.com>
2025-02-04 22:28:30 -0500

    Run DD Synthetic tests of course authoring pipeline

--- src/main/java/com/2u/synthetic-testing/SyntheticTestRequest.java
+++
@@ -1,9 +0,0 @@
-class SyntheticTestRequest {
-    public String testName;
-    public String testId;
-
-    public SyntheticTestRequest(String testName, String testId) {
-        this.testName = testName;
-        this.testId = testId;
-    }
-}
